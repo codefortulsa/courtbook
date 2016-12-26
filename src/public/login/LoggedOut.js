f7206107854cd6362a68dc6a262e71eb0da08bae
Ash Lux <arlux@monsanto.com>
2016-12-26 15:31:24 -0600

    Use alert showing user is now logged out.

--- src/public/login/LoggedOut.js
+++ src/public/login/LoggedOut.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Login from './Login';
+import {Alert} from 'react-bootstrap';
 
 const LoggedOut = ({auth}) => (
     <div>
-        You have been logged out.
+        <Alert bsStyle="success">You have been logged out.</Alert>
         <Login auth={auth}/>
     </div>
 );
