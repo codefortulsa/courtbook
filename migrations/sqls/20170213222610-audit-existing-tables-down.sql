14af5089acf3fa1dc0097455727414b34296b101
Ash Lux <arlux@monsanto.com>
2017-02-13 22:51:45 -0600

    Audit existing tables: courtCases, courtCaseEvents, stakeholders.

---
+++ migrations/sqls/20170213222610-audit-existing-tables-down.sql
@@ -0,0 +1,19 @@
+DROP TRIGGER IF EXISTS audit_trigger_row
+ON courtbook_db.public."courtCaseEvents";
+
+DROP TRIGGER IF EXISTS audit_trigger_stm
+ON courtbook_db.public."courtCaseEvents";
+
+DROP TRIGGER IF EXISTS audit_trigger_row
+ON courtbook_db.public."courtCases";
+
+DROP TRIGGER IF EXISTS audit_trigger_stm
+ON courtbook_db.public."courtCases";
+
+DROP TRIGGER IF EXISTS audit_trigger_row
+ON courtbook_db.public."stakeholders";
+
+DROP TRIGGER IF EXISTS audit_trigger_stm
+ON courtbook_db.public."stakeholders";
+
+
