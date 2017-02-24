4eb0014d58ed3d06ea310de4d8ba35098506988d
Ash Lux <arlux@monsanto.com>
2017-02-23 19:42:03 -0600

    Fix bypass auth.  Debug req and tokenPayload.

--- src/server/auth-middleware.js
+++ src/server/auth-middleware.js
@@ -2,6 +2,8 @@ import _ from "lodash";
 import expressJwt from "express-jwt";
 
 export const secret = (req, tokenPayload, cb) => {
+    console.info("req", req);
+    console.info("tokenPayload", tokenPayload);
     if (_.includes(tokenPayload.aud, process.env.AUTH0_COURTBOT_UI_CLIENT_ID)) {
         cb(null, process.env.AUTH0_COURTBOT_UI_CLIENT_SECRET);
     } else {
@@ -13,7 +15,4 @@ const bypassAuthentication = (err, req, res, next) => next();
 
 const jwtAuthentication = expressJwt({secret});
 
-console.info("bypass auth env=", process.env.BYPASS_AUTH);
-console.info("bypass auth=", process.env.BYPASS_AUTH === true);
-
-export default process.env.BYPASS_AUTH === true ? bypassAuthentication : jwtAuthentication;
+export default process.env.BYPASS_AUTH === 'true' ? bypassAuthentication : jwtAuthentication;
