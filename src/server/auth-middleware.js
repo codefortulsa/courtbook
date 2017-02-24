16fd8d6772fb169d1df903db0eb6ff0113fdbacf
Ash Lux <arlux@monsanto.com>
2017-02-23 20:00:26 -0600

    Test with just one token...

--- src/server/auth-middleware.js
+++ src/server/auth-middleware.js
@@ -1,18 +1,7 @@
-import _ from "lodash";
 import expressJwt from "express-jwt";
-
-export const secret = (req, tokenPayload, cb) => {
-    console.info("req", req);
-    console.info("tokenPayload", tokenPayload);
-    if (_.includes(tokenPayload.aud, process.env.AUTH0_COURTBOT_UI_CLIENT_ID)) {
-        cb(null, process.env.AUTH0_COURTBOT_UI_CLIENT_SECRET);
-    } else {
-        cb(null, process.env.AUTH0_COURTBOT_SIGNING_CERT);
-    }
-};
 
 const bypassAuthentication = (err, req, res, next) => next();
 
-const jwtAuthentication = expressJwt({secret});
+const jwtAuthentication = expressJwt({secret: process.env.AUTH0_COURTBOT_UI_CLIENT_SECRET});
 
 export default process.env.BYPASS_AUTH === 'true' ? bypassAuthentication : jwtAuthentication;
